455b0cf87cbcd36596b5c0e2b775c4791201bef2
venturads <albert@venturads.com>
2020-07-30 12:50:50 -0700

    Updates

---
+++ precache-manifest.14ab70f031dcafe3ee1cfc510bbd8879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea051984327d583f1b794aa2f7f03e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44909fcd63555be95bf3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8abe8c41c66da314889f",
+    "url": "/static/js/2.68cda3af.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.68cda3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44909fcd63555be95bf3",
+    "url": "/static/js/main.b2f4d289.chunk.js"
+  },
+  {
+    "revision": "3d150f4c2bde87d490ca",
+    "url": "/static/js/runtime-main.44983115.js"
+  }
+]);
